3a564332fe0e73553322dd52e98c122408281b14
Kenny Lau <klau20@stuy.edu>
2022-01-13 11:43:17 -0500

    Completed select for Main Server. Began select for sub-server

--- server.c
+++ server.c
@@ -52,6 +52,50 @@ int send_to_server(char * message, int pipe_write){
 int subserver_handler(int client_socket, int pipe_read, int pipe_write){
   printf("Sub-Server created\n");
 
+  fd_set read_descriptors;
+
+  int max_descriptor;
+  if(client_socket > pipe_read){
+    max_descriptor = client_socket;
+  } else {
+    max_descriptor = pipe_read;
+  }
+
+  while(1){
+    FD_ZERO(&read_descriptors);
+    FD_SET(client_socket, &read_descriptors);
+    FD_SET(pipe_read, &read_descriptors);
+
+    int s = select(max_descriptor + 1, &read_descriptors, NULL, NULL, NULL);
+
+    if (FD_ISSET(client_socket, &read_descriptors)){
+      char * message = calloc(BUFFER_SIZE, 1);
+      int bytes_read = read(client_socket, message, BUFFER_SIZE);
+
+      if (bytes_read != 0){
+        printf("[%d] Sub-Server has read from Client: %s\n", getpid(), message);
+        send_to_server(message, pipe_write);
+      } else {
+        printf("Client not connected\n");
+      }
+
+      free(message);
+    } else {
+      char * message = calloc(BUFFER_SIZE, 1);
+      int bytes_read = read(pipe_read, message, BUFFER_SIZE);
+
+      if (bytes_read != 0){
+        printf("[%d] Sub-Server has read from Server: %s\n", getpid(), message);
+        send_to_client(client_socket, message);
+      } else {
+        printf("Server not connected\n");
+      }
+
+      free(message);
+    }
+  }
+
+  /*
   char * message = calloc(BUFFER_SIZE, 1);
 
   int f = fork();
@@ -71,8 +115,25 @@ int subserver_handler(int client_socket, int pipe_read, int pipe_write){
   } else {
     //subserver_server(client_socket, pipe_read);
   }
+  */
 
   exit(0);
+}
+
+int send_to_subserver(char * message, int pipe_write){
+  write(pipe_write, message, BUFFER_SIZE);
+
+  return 0;
+}
+
+int send_to_all(char * message, int pipes[10]){
+  int i;
+  for (i = 0; i < 10; i++){
+    printf("%d\n", pipes[i]);
+    if (pipes[i] > -1){
+      send_to_subserver(message, pipes[i]);
+    }
+  }
 }
 
 //main server sets up subservers
@@ -117,6 +178,15 @@ int main(){
       int fds2[2]; //SERVER WRITE
       pipe(fds2);
 
+      v = 0;
+      for (v = 0; v < 10; v++){
+        if (subserver_write_pipes[v] == -1){
+          subserver_write_pipes[v] = fds2[0];
+          //printf("added %d\n", subserver_read_pipes[v]);
+          break;
+        }
+      }
+
       int f = fork();
 
       if (f == 0){ //subserver
@@ -134,9 +204,13 @@ int main(){
     for (i = 0; i < 10; i++){
       //printf("checking %d\n", subserver_read_pipes[i]);
       if (subserver_read_pipes[i] != -1 && FD_ISSET(subserver_read_pipes[i], &read_descriptors)){
-        printf("Read from SubServer!\n");
+        //printf("Read from SubServer!\n");
         char * message = calloc(BUFFER_SIZE, 1);
         read(subserver_read_pipes[i], message, BUFFER_SIZE);
+        printf("[%d] Server read from Sub-Server: %s\n", getpid(), message);
+        send_to_all(message, subserver_write_pipes);
+
+        free(message);
       }
     }
 
