34d9d0970375c0a61449e6e08e9ea07b2b1a0d94
lauk20 <klau20@stuy.edu>
2022-01-23 17:54:34 -0500

    Removed unecessary check

--- client.c
+++ client.c
@@ -43,7 +43,7 @@ int main(int argc, char ** args){
 
   socket = client_connect(address);
 
-  if (errno || socket < 0){
+  if (socket < 0){
     printf("Error Connecting to Server. If you are not on the same machine as the Server, please run \"./client [SERVER ADDRESS HERE]\", replacing \"SERVER ADDRESS HERE\" with the Server's address and removing the brackets.\n");
     return -1;
   }
